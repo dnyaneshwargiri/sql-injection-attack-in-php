7c7331d71e727b7961fe8c6291b267216332a114
dnyaneshwar giri <34788823+dnyaneshwargiri@users.noreply.github.com>
2019-04-01 22:26:58 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,8 @@
 <?php
+   /*replace parameter's values with your local or remote database values */
    define('DB_SERVER', 'localhost:3306');
    define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', '');
-   define('DB_DATABASE', 'ics');
+   define('DB_PASSWORD', 'root');
+   define('DB_DATABASE', 'sqlinjectiondemo');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
