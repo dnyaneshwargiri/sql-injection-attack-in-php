94a995bce42625e8b8a6e81caca905f66a788a66
dnyaneshwar giri <34788823+dnyaneshwargiri@users.noreply.github.com>
2019-04-01 22:31:32 +0530

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -9,28 +9,23 @@ Date:26-03-2019
    <head>
       <title>Welcome </title>
    </head>
-
    <body>
       <h1>Welcome <?php echo $login_session; ?></h1>
       <h2><a href = "logout.php">Sign Out</a></h2>
 		  <?php
 		  $rows;
+	   		/*Displaying sql malicious query and its result for demo purpose*/
 		  echo('SQL Query  - '.$_SESSION['sql_query'].'</br>');
 		  echo('Number of records is '.$_SESSION['count'].'</br>');
 		  if(isset($_SESSION['query_result']))
 		  {
 		  $rows = $_SESSION['query_result'];
-		  //var_dump($rows);
 	      }
 
 		 for($i=0;$i<count($rows);$i++)
-		  {  /*foreach($rows[$keys[$i]] as $k => $v)
-				  {		 echo("  	".$k." 	 ".$v);
-				  }*/
-				  print_r($rows[$i]);
+		  {	 print_r($rows[$i]);
 			 echo("<br/>");
 		  }
 		  ?>
    </body>
-
 </html>
